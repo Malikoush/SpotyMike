66e12156fde92b0d52f51c5e10ced7b332117ffc
Lucien Lafaye <lafaye@et.intechinfo.fr>
2024-03-27 13:50:26 +0100

    Add controller

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LoginController extends AbstractController
+{
+
+    private $repository;
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+        $this->repository = $entityManager->getRepository(User::class);
+    }
+
+    #[Route('/login', name: 'app_login', methods: 'GET')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to MikeLand',
+            'path' => 'src/Controller/LoginController.php',
+            ]);
+        }
+        
+    // use Symfony\Component\HttpFoundation\Request;
+    #[Route('/login', name: 'app_login_post', methods: ['POST', 'PUT'])]
+    public function login(Request $request): JsonResponse
+    {
+
+        $user = $this->repository->findOneBy(["email" => "mike.sylvestre@lyknowledge.io"]);
+        return $this->json([
+            'user' => json_encode($user),
+            'data' => $request->getContent(),
+            'message' => 'Welcome to MikeLand',
+            'path' => 'src/Controller/LoginController.php',
+        ]);
+    }
+}
